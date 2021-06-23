f6dd50354d7448d545e35fef0d7fc01693bdc9a4
BM <bmfish@bm.com>
2021-06-24 00:49:05 +0800

    eeee

--- src/main/java/wk/algorithm/StringSubAppearCount.java
+++ src/main/java/wk/algorithm/StringSubAppearCount.java
@@ -10,7 +10,9 @@
 
 package wk.algorithm;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class StringSubAppearCount {
@@ -20,7 +22,10 @@ public class StringSubAppearCount {
         //子字符串出现的次数
         //subStrAppearCount("jin tian shi ge hao ri zi ,jiushi jin tian ya ,jintian","jin");
         //公共字符串
-        getMaxSameString();
+      //  getMaxSameString();
+
+        //最大不重复子串
+        maxLengsubString();
     }
 
     /**
@@ -65,13 +70,33 @@ public class StringSubAppearCount {
      */
     public static void maxLengsubString(){
 
-        String s="abcdefcacedafghiklmnc";
-        String con;
-        Set set =new HashSet();
+        String s="abca1234321gfds3";
+        String con=null;
+        Set set =new LinkedHashSet();
         int j=0;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length()-1;i++){
+            if(i!=0){
+                set.remove(s.charAt(i-1));
+            }
+            while(j<s.length()&&!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+            }
+          //  System.out.println(set.size());
+            if(con==null||con.length()<set.size()) {
+                StringBuffer sb=new StringBuffer();
+                for (Object o : set) {
+                    sb.append(o);
+                }
+                con=sb.toString();
+             //   System.out.println(con);
+            }
 
-
+            if(j==s.length())
+                break;
         }
+       System.out.println(con);
     }
+
+
 }
